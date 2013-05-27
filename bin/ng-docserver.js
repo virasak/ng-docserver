e4f0ec87672f44b1c8abd1cdec9cc89eb05bbb39
Virasak Dungsrikaew <virasak@gmail.com>
2013-05-28 04:09:02 +0700

    change coding style to simplify the control flow

--- bin/ng-docserver.js
+++ bin/ng-docserver.js
@@ -5,39 +5,28 @@ if (process.argv.length < 3) {
     return;
 }
 
-var zipfile = process.argv[2],
-    port = Number(process.argv[3]) || 1337,
-    path = require('path'),
-    http = require('http'),
-    url = require('url'),
-    mime = require('mime'),
-    docZip = require('adm-zip')(zipfile),
-    dirname = path.basename(zipfile, '.zip');
+var file    = process.argv[2],
+    port    = Number(process.argv[3]) || 1337,
+    zip     = require('adm-zip')(file),
+    dirname = require('path').basename(file, '.zip'),
+    mime    = require('mime'),
+    url     = require('url'),
+    http    = require('http');
 
-http.createServer(function(req, res){
-    var uri = url.parse(req.url).pathname,
-        filePath = dirname + uri;
-
-    if (['/', '/docs', '/docs/'].indexOf(uri) > -1) {
-        res.writeHead(302, {'Location': '/docs/index.html'});
-        res.end();
-        return;
-    }
-
-    docZip.readFileAsync(filePath, function(data){
-        if (data === null) {
-            if (uri.indexOf('/docs/') === 0 && uri.indexOf('/docs/index.html#!') !== 0) {
-                res.writeHead(302, {'Location': '/docs/index.html#!' + uri.substr(5)});
-                res.end();
-            } else {
-                res.writeHead(404);
-                res.end();
-            }
-            return;
+http.createServer(function(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    zip.readFileAsync(dirname + pathname, function(data) {
+        if (data) {
+            res.writeHead(200, {'Content-Type': mime.lookup(pathname)});
+            res.write(data);
+        } else if (['/', '/docs', '/docs/'].indexOf(pathname) > -1) {
+            res.writeHead(302, {'Location': '/docs/index.html'});
+        } else if (pathname.indexOf('/docs/') === 0) {
+            res.writeHead(302, {'Location': '/docs/index.html#!' + pathname.substr(5)});
+        } else {
+            res.writeHead(404);
         }
-
-        res.writeHead(200, {'Content-Type': mime.lookup(uri)});
-        res.end(data);
+        res.end();
     });
 }).listen(port);
 
